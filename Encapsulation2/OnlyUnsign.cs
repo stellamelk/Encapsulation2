3214203e847b1543ca0e7f6bc595e162d1ada7fd
Stella Melkonyan <stella.melkonyan.benivo@outlook.com>
2021-11-12 01:07:39 +0400

    Add project files.

---
+++ Encapsulation2/OnlyUnsign.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation2
+{
+    class OnlyUnsign
+    {
+        private int _x;
+
+        public int getX()
+        {
+            return _x;
+        }
+
+        public void setX(int x)
+        {
+            if (x > 0)
+            {
+                _x = x;
+            }
+            else
+            {
+                Console.WriteLine("Please pass the positive value");
+            }
+        }
+    }
+}
---
+++ Encapsulation2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlyUnsign e = new OnlyUnsign();
+            e.setX(10);
+            Console.WriteLine(e.getX());
+            e.setX(-50);
+            Console.WriteLine(e.getX());
+            Console.WriteLine("Press any key to exist");
+            Console.ReadKey();
+        }
+    }
+}
